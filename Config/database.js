f15fdf103a8cef7712cbf394a48d8a0984287ff2
Daniel Borghi <dann.borghi@gmail.com>
2024-08-26 17:06:39 -0300

    Segundo Commit

---
+++ Config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host:'localhost',
+    username: 'postgres',
+    password: 'daniel21',
+    database: 'apinode',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
