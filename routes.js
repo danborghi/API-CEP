b12fc0f048750b71294f3c5a42d3da9aedd26c78
Daniel Borghi <dann.borghi@gmail.com>
2024-08-29 10:55:48 -0300

    Funcionando o CreateEnderecoCep!

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const enderecoController = require('./controllers/EnderecoController')
 const router = express.Router();
 
 router.post('/enderecos', enderecoController.createEndereco);
+router.post('/enderecos/:cep', enderecoController.createEnderecoCep);
 router.get('/enderecos', enderecoController.getAllEnderecos);
 router.get('/enderecos/:Id', enderecoController.getEnderecoById);
 router.put('/enderecos/:Id', enderecoController.updateEndereco);
